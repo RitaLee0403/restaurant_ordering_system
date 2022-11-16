42f94529b820936ea4c435f43663c79609118e52
mayonnaise0403 <rita09436@gmail.com>
2022-11-16 18:09:31 +0800

    修改老師要求

--- api_fun.py
+++ api_fun.py
@@ -9,7 +9,7 @@ pool = pooling.MySQLConnectionPool(
         host = 'localhost',
         database = 'taipei_attraction',
         user = 'root',
-        password = "",
+        password = "0403",
         charset='utf8',
         auth_plugin='mysql_native_password'
 
@@ -34,18 +34,17 @@ class ConnectToSql:
     def get_pc(self, index, id = None):
         pcArr = []
         if(id == None):
-            for i in range(1):
-                cnx = pool.get_connection()
-                cursor = cnx.cursor()
-                execute = "SELECT `picture`.`pc` from `picture` INNER JOIN `data` ON `data`.`idName` = `picture`.`id` AND `data`.`idName` = %s;"
-                values = ([f"{index}"])
-                cursor.execute(execute,values)
-                records = cursor.fetchall()
-                for k in records:
-                    pcArr.append(k[0])
-                index+=1
-                cursor.close()
-                cnx.close()
+            cnx = pool.get_connection()
+            cursor = cnx.cursor()
+            execute = "SELECT `picture`.`pc` from `picture` INNER JOIN `data` ON `data`.`idName` = `picture`.`id` AND `data`.`idName` = %s;"
+            values = ([f"{index}"])
+            cursor.execute(execute,values)
+            records = cursor.fetchall()
+            for k in records:
+                pcArr.append(k[0])
+            index+=1
+            cursor.close()
+            cnx.close()
             return pcArr
         else:
             cnx = pool.get_connection()
@@ -97,32 +96,30 @@ class ConnectToSql:
         index = (12 * page) + 1
         imageIndex = index
         if(keyword == ""):
-            for i in range(pages):
-                cnx = pool.get_connection()
-                cursor = cnx.cursor()
-                execute = "SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`MRT`,`latitude`,`longitude` FROM `data` WHERE `idName` = %s;"
-                values = ([f"{index}"])
-                cursor.execute(execute,values)
-                record = cursor.fetchall()
-                index +=1
-                for k in record:
-                    images = self.get_pc(imageIndex)
-                    arr.append({
-                        "id" : k[0],
-                        "name" : k[1],
-                        "category":k[2],
-                        "description":k[3],
-                        "address":k[4],
-                        "transport":k[5],
-                        "mrt":k[6],
-                        "lat":k[7],
-                        "lng":k[8],
-                        "images":images
-                    })
-                    imageIndex +=1
-                    
-                cursor.close()
-                cnx.close()
+            cnx = pool.get_connection()
+            cursor = cnx.cursor()
+            execute = "SELECT `id`,`name`,`category`,`description`,`address`,`transport`,`MRT`,`latitude`,`longitude` FROM `data` WHERE idName >= %s ORDER BY `idName` LIMIT %s;"
+            values = (f"{index}",pages)
+            cursor.execute(execute,values)
+            record = cursor.fetchall()
+            print(record)
+            for k in record:
+                images = self.get_pc(imageIndex)
+                arr.append({
+                    "id" : k[0],
+                    "name" : k[1],
+                    "category":k[2],
+                    "description":k[3],
+                    "address":k[4],
+                    "transport":k[5],
+                    "mrt":k[6],
+                    "lat":k[7],
+                    "lng":k[8],
+                    "images":images
+                })
+                imageIndex +=1
+            cursor.close()
+            cnx.close()
         else:
             cnx = pool.get_connection()
             cursor = cnx.cursor()
