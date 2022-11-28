47b03c01ee3e53c824b7ee145ef4bda6426e17e2
mayonnaise0403 <rita09436@gmail.com>
2022-11-28 15:29:15 +0800

    week3

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ getData = ConnectToSql()
 def index():
 	return render_template("index.html")
 
-@app.route("/attraction")
-def showAttraction():
+@app.route("/attraction/<id>")
+def showAttraction(id):
     return render_template("attraction.html")
 
 @app.route("/api/attraction/<id>")
