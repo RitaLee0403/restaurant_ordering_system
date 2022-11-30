068a55cf4c4bba380da64488ef5e867fbc376db0
mayonnaise0403 <rita09436@gmail.com>
2022-11-30 10:45:18 +0800

    add blueprint

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import *
-from api_fun import *
+from flask import Flask, render_template
+from view.api import api_blueprint
+
+
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
-getData = ConnectToSql()
+
 # Pages
 @app.route("/")
 def index():
@@ -13,91 +15,15 @@ def index():
 def showAttraction(id):
     return render_template("attraction.html")
 
-@app.route("/api/attraction/<id>")
-def attraction(id):
-	id = int(id)
-	try:
-		if(len(getData.getAttraction(id)) == 0):
-			data = {
-				"error":True,
-				"message":"景點編號不正確"
-			}
-			return data,400
-		data = {
-			"data":getData.getAttraction(id)
-		}
-		return data,200
-	except:
-		data = {
-			"error":True,
-			"message":"連線失敗"
-		}
-		return data,500
-
-
-
-@app.route("/api/attractions/")
-def apiAttraction():
-	try:
-		page = request.args.get("page","0")
-		keyword = request.args.get("keyword", "")
-		page=int(page)
-		if(len(getData.showPage(page+1)) == 0):
-			nextPage = None
-		else:
-			nextPage = page +1
-
-		if(page < 0 or page > 4):
-			data = {
-				"error": True,
-				"message": "無此頁"
-			}
-			return data,200
-		if(keyword != ""):
-			if(len(getData.showPage(page+1,keyword)) == 0):
-				nextPage = None
-				
-			data = {
-				"nextPage":nextPage,
-				"data":getData.showPage(page,keyword)
-			}
-			return data,200
-		if(keyword == ""):
-			data = {
-					"nextPage":nextPage,
-					"data":getData.showPage(page)
-			}
-			return data,200
-	except:
-			data = {
-				"error": True,
-				"message":"連線失敗"
-			}
-			return data,500
-
-
-     
-@app.route("/api/categories")
-def apiCategories():
-	try:
-		data = {
-				"data":getData.getCategories()	
-			}
-		return data,200
-	except:
-		data = {
-				"error": True,
-				"data":"連線失敗"	
-				}
-		return data,500
-		
-	
 
 @app.route("/booking")
 def booking():
 	return render_template("booking.html")
+
 @app.route("/thankyou")
 def thankyou():
 	return render_template("thankyou.html")
+
+app.register_blueprint(api_blueprint)
 
 app.run(host = "0.0.0.0",port=3000,debug=True)
