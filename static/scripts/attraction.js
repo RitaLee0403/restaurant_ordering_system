06da971d5cb4a928c5a8df606efe3cc00ef39a32
mayonnaise0403 <rita09436@gmail.com>
2023-03-08 21:37:15 +0800

    fix:非同步problem

--- static/scripts/attraction.js
+++ static/scripts/attraction.js
@@ -17,75 +17,41 @@ let pcPosition = 0;
 let orderBtn = document.querySelector(".order-btn");
 let date = document.querySelector(".date");
 let datetime = new Date();
-let year,month,day,time,orderData,price;
-let id = location.pathname.replace("/attraction/","");
+let year, month, day, time, orderData, price;
+let id = location.pathname.replace("/attraction/", "");
 let bookingMessage = document.querySelector(".booking-message");
 let darker = document.querySelector(".darker");
 let bookingMessageClose = document.querySelector(".booking-message-btn");
 let bookingMessageFont = document.querySelector(".booking-message-font");
 let title = document.querySelector(".title");
 let category = document.querySelector(".category");
-let  pcBackground = document.querySelector(".pc-background");
+let pcBackground = document.querySelector(".pc-background");
 let preloadingPc = document.querySelector(".lds-dual-ring");
 
 
 
 
-//圖片轉播圖
-arrowRight.addEventListener("click",function(){
-    allPc = document.querySelectorAll(".content-pc");
-    allRadio = document.querySelectorAll(".dot");
-    if(pcPosition === allPc.length - 1){
-        allPc[pcPosition].style.display = "none";
-        allPc[0].style.display = "block";
-        allRadio[0].checked = true;
-        pcPosition = 0;
-        return;
-    }
-    allPc[pcPosition].style.display = "none";
-    allPc[pcPosition + 1].style.display = "block";
-    allRadio[pcPosition + 1].checked = true;
-    pcPosition ++;
-});
-
-
-arrowLeft.addEventListener("click",()=>{
-    allPc = document.querySelectorAll(".content-pc");
-    allRadio = document.querySelectorAll(".dot");
-    if(pcPosition === 0){
-        allPc[pcPosition].style.display = "none";
-        allPc[allPc.length - 1].style.display = "block";
-        allRadio[allPc.length - 1].checked = true;
-        pcPosition = allPc.length - 1;
-        return;
-        
-    }
-    allPc[pcPosition].style.display = "none";
-    allPc[pcPosition - 1].style.display = "block";
-    allRadio[pcPosition - 1].checked = true;
-    pcPosition --;
-})
 
 
 //點擊下面圓圓切換圖片
-function clickDotToChangePc(){
+function clickDotToChangePc() {
     radio = document.querySelectorAll(".dot");
-    for(let i = 0; i < radio.length; i++){//讓每個點點 添加click事件
-        radio[i].addEventListener("click",function(){ 
+    for (let i = 0; i < radio.length; i++) {//讓每個點點 添加click事件
+        radio[i].addEventListener("click", function () {
             allRadio = document.querySelectorAll(".dot");
             allPc = document.querySelectorAll(".content-pc");
-            for(let k = 0; k < allRadio.length; k++){
-                if(allPc[k].style.display === "block"){
+            for (let k = 0; k < allRadio.length; k++) {
+                if (allPc[k].style.display === "block") {
                     allPc[k].style.display = "none";
                 }
-                
-                if(allRadio[k].checked === true){
-                    if(k != 0){ //一開始載入畫面第0個圖是block，要先把它關掉
+
+                if (allRadio[k].checked === true) {
+                    if (k != 0) { //一開始載入畫面第0個圖是block，要先把它關掉
                         allPc[0].style.display = "none";
                     }
                     pcPosition = k;
                 }
-                
+
             }
             allPc[pcPosition].style.display = "block";
         })
@@ -94,134 +60,171 @@ function clickDotToChangePc(){
 
 //取得圖片並加入點點
 fetch(url)
-.then((response)=>{
-    return response.json()
-})
-.then((data)=>{
-    data = data["data"];
-    pcCount = data["images"].length;
-    for(let i = 0; i < data["images"].length; i++){
-        let newDot = document.createElement("input");
-        newDot.type = "radio";
-        newDot.name = "control";
-        newDot.className = "dot";
-        pictureDot.appendChild(newDot);
-        let newImg = document.createElement("img");
-        newImg.onload = ()=>{
-            contentLeft.insertBefore(newImg,arrowLeft);
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        data = data["data"];
+        pcCount = data["images"].length;
+        for (let i = 0; i < data["images"].length; i++) {
+            let newDot = document.createElement("input");
+            newDot.type = "radio";
+            newDot.name = "control";
+            newDot.className = "dot";
+            pictureDot.appendChild(newDot);
+            let newImg = document.createElement("img");
+            newImg.onload = () => {
+                contentLeft.insertBefore(newImg, arrowLeft);
+            }
+            pcBackground.style.display = "none";
+            newImg.src = data["images"][i];
+            arrowLeft.style.display = "block";
+            arrowRight.style.display = "block";
+            newImg.className = "content-pc";
+
+
+            if (i > 0) {
+                newImg.style.display = "none";
+            }
+
         }
-        pcBackground.style.display = "none";
-        newImg.src = data["images"][i];
-        arrowLeft.style.display = "block";
-        arrowRight.style.display = "block";
-        newImg.className = "content-pc";
-        
-        
-        if(i > 0){
-            newImg.style.display="none";
+
+        year = datetime.getFullYear();
+        month = datetime.getMonth() + 1;
+        day = datetime.getDate();
+        if (datetime.getDate() < 10) {
+            day = `0${day}`;
         }
-    
-    }
-    
-    year = datetime.getFullYear();
-    month = datetime.getMonth() + 1;
-    day = datetime.getDate();
-    if(datetime.getDate() < 10){
-        day = `0${day}`;
-    }
-    if(datetime.getMonth() < 9){
-        month = `0${month + 1}`;
-    }
-    date.value = `${year}-${month}-${day}`;  //設定input type="datetime" 的初始值
-    document.title = data["name"];
-    radio = document.querySelector(".dot");
-    radio.checked = true;  //先讓圓圓在第一個
-    title.innerHTML = data["name"];
-    category.innerHTML = `${data["category"]} at ${data["mrt"]}`;
-    descriptionFont.innerHTML = data["description"];
-    address.innerHTML = data["address"];
-    transport.innerHTML = data["transport"];
-    clickDotToChangePc();
-})
+        if (datetime.getMonth() < 9) {
+            month = `0${month + 1}`;
+        }
+        date.value = `${year}-${month}-${day}`;  //設定input type="datetime" 的初始值
+        document.title = data["name"];
+        radio = document.querySelector(".dot");
+        radio.checked = true;  //先讓圓圓在第一個
+        title.innerHTML = data["name"];
+        category.innerHTML = `${data["category"]} at ${data["mrt"]}`;
+        descriptionFont.innerHTML = data["description"];
+        address.innerHTML = data["address"];
+        transport.innerHTML = data["transport"];
+        clickDotToChangePc();
+    })
+    .then(() => {
+        //圖片轉播圖
+        arrowRight.addEventListener("click", function () {
+            allPc = document.querySelectorAll(".content-pc");
+            allRadio = document.querySelectorAll(".dot");
+            if (pcPosition === allPc.length - 1) {
+                allPc[pcPosition].style.display = "none";
+                allPc[0].style.display = "block";
+                allRadio[0].checked = true;
+                pcPosition = 0;
+                return;
+            }
+            allPc[pcPosition].style.display = "none";
+            allPc[pcPosition + 1].style.display = "block";
+            allRadio[pcPosition + 1].checked = true;
+            pcPosition++;
+        });
+
+
+        arrowLeft.addEventListener("click", () => {
+            allPc = document.querySelectorAll(".content-pc");
+            allRadio = document.querySelectorAll(".dot");
+            if (pcPosition === 0) {
+                allPc[pcPosition].style.display = "none";
+                allPc[allPc.length - 1].style.display = "block";
+                allRadio[allPc.length - 1].checked = true;
+                pcPosition = allPc.length - 1;
+                return;
+
+            }
+            allPc[pcPosition].style.display = "none";
+            allPc[pcPosition - 1].style.display = "block";
+            allRadio[pcPosition - 1].checked = true;
+            pcPosition--;
+        })
+
+    })
 
 
 
 
 //點擊預定行程
-orderBtn.addEventListener("click",()=>{
-    if(datetimeInput[0].checked == true){
+orderBtn.addEventListener("click", () => {
+    if (datetimeInput[0].checked == true) {
         time = "morning";
         price = "2000元"
-    }else{
+    } else {
         time = "afternoon";
         price = "2500元";
     }
-    fetch("/api/booking",{
-        method : "POST",
-        body:JSON.stringify({
-            "attractionId" : `${id}`,
-            "date" : `${date.value}`,
-            "time" : time,
-            "price" : price
+    fetch("/api/booking", {
+        method: "POST",
+        body: JSON.stringify({
+            "attractionId": `${id}`,
+            "date": `${date.value}`,
+            "time": time,
+            "price": price
         }),
-        headers:{
-            'Content-type':'application/json; charset=UTF-8',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    .then((response)=>{
-        return response.json();
-    })
-    .then((data)=>{
-        if("ok" in data){
-            bookingMessage.style.display = "block";
-            darker.style.display = "block";
-            fetch("/api/booking") //取得購物車訂單數量
-            .then((response)=>{
-                return response.json()
-            })
-            .then((data)=>{
-                productCount.innerHTML = data.data.length;
-            })
-            
-        }
-        if("error" in data && data.message === "日期輸入錯誤"){
-            bookingMessage.style.display = "block";
-            darker.style.display = "block";
-            bookingMessageFont.innerHTML = "日期輸入錯誤";
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if ("ok" in data) {
+                bookingMessage.style.display = "block";
+                darker.style.display = "block";
+                fetch("/api/booking") //取得購物車訂單數量
+                    .then((response) => {
+                        return response.json()
+                    })
+                    .then((data) => {
+                        productCount.innerHTML = data.data.length;
+                    })
 
-        }
-        if("error" in data && data.message === "尚未登入"){
-            bookingMessage.style.display = "block";
-            darker.style.display = "block";
-            bookingMessageFont.innerHTML = "請先登入再預約";
+            }
+            if ("error" in data && data.message === "日期輸入錯誤") {
+                bookingMessage.style.display = "block";
+                darker.style.display = "block";
+                bookingMessageFont.innerHTML = "日期輸入錯誤";
 
-        }
-        if("error" in data && data.message === "伺服器內部錯誤"){
-            bookingMessage.style.display = "block";
-            darker.style.display = "block";
-            bookingMessageFont.innerHTML = "請再重試一次";
+            }
+            if ("error" in data && data.message === "尚未登入") {
+                bookingMessage.style.display = "block";
+                darker.style.display = "block";
+                bookingMessageFont.innerHTML = "請先登入再預約";
 
-        }
-    })
-   
+            }
+            if ("error" in data && data.message === "伺服器內部錯誤") {
+                bookingMessage.style.display = "block";
+                darker.style.display = "block";
+                bookingMessageFont.innerHTML = "請再重試一次";
+
+            }
+        })
+
 })
 
 
 
-bookingMessageClose.addEventListener("click",()=>{
+bookingMessageClose.addEventListener("click", () => {
     bookingMessage.style.display = "none";
     darker.style.display = "none";
-    if(bookingMessageFont.innerHTML === "日期輸入錯誤"){
+    if (bookingMessageFont.innerHTML === "日期輸入錯誤") {
         location.reload();
     }
 })
 
 //顯示費用
-selectMorning.addEventListener("click",()=>{
-    costNum.innerHTML = "2000元"; 
+selectMorning.addEventListener("click", () => {
+    costNum.innerHTML = "2000元";
 })
 
-selectAfternoon.addEventListener("click",()=>{
+selectAfternoon.addEventListener("click", () => {
     costNum.innerHTML = "2500元";
 })
 
